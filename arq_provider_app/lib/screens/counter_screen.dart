001679d543d16a72fc06a702e4d4308149834b1a
aromanf96 <aromanf96@gmail.com>
2024-08-14 11:30:08 -0500

    Counter with provider

---
+++ arq_provider_app/lib/screens/counter_screen.dart
@@ -0,0 +1,48 @@
+import 'package:arq_provider_app/provider/counter_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter with Provider'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Consumer<CounterProvider>(
+                builder: (context, counterProvider, child) {
+              return Text('${counterProvider.count}',
+                  style: Theme.of(context).textTheme.headlineMedium);
+            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: () => context.read<CounterProvider>().increment(),
+                  tooltip: 'Increment',
+                  heroTag: null,
+                  child: const Icon(Icons.add),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                FloatingActionButton(
+                  onPressed: () => context.read<CounterProvider>().decrement(),
+                  tooltip: 'Decrement',
+                  heroTag: null,
+                  child: const Icon(Icons.remove),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
